7482c80711a7f2fdd47b082313db260d70389606
Krystian Wieczorek <krystianwieczorek70@gmail.com>
2021-12-21 14:45:42 +0100

    Commit

--- src/pl/akademia108/Main.java
+++ src/pl/akademia108/Main.java
@@ -3,8 +3,20 @@ package pl.akademia108;
 public class Main {
 
     public static void main(String[] args) {
-	byte number;
-            number = 127;
+
+        byte varByte = 127;
+
+        short sumShort = (short) (varByte + 2);
+        System.out.println("sumShort: " + sumShort);
+
+        byte minusByte = (byte) (varByte - 2);
+        System.out.println("minusByte: " + minusByte);
+
+        byte divideByte = (byte) (varByte / 2);
+        System.out.println("divideByte: " + divideByte);
+
+        short multipleShort = (short) (varByte * 2);
+        System.out.println("multipleShort: " + multipleShort);
 
     }
 }
